669f74fbd09d2e7aa2be3a562876ef4188082bbd
plappalainen <petteri.lappalainen@zalando.fi>
2018-06-26 17:23:43 +0300

    new files

---
+++ gmane/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Month','umich.edu','uct.ac.za','columbia.edu','mac.com','indiana.edu','berkeley.edu','unl.edu','mtu.edu','ucdavis.edu','gmail.com'],
+['2005-12',57,14,13,12,12,12,12,11,11,10]
+];
